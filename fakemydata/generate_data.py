14963fd3478d4fe54872c152e53bc53e6a6c528f
emmarocheteau <ecr38@cam.ac.uk>
2017-11-25 18:55:23 +0000

    generate_data function complete. Precision needs to be sorted at the front end

--- fakemydata/generate_data.py
+++ fakemydata/generate_data.py
@@ -1,26 +1,32 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import json
 
-#User defined variables
-xlow = 0
-xhigh = 5
-xinterval = "random" #user puts in either "random" or "regular"
-numberofdatapoints = 10
-precision = 3 #decimal precision
-noiselevel =  0.08 #user defines this with a slider
-idealcurve="8*x**2 + 5*x + 6"
+def generate_data(xlow, xhigh, xinterval, numberofdatapoints, noiselevel, idealcurvecode):
 
-if xinterval == "regular":
-    x = np.linspace(xlow, xhigh, num=numberofdatapoints)
-if xinterval == "random":
-    x = np.random.uniform(xlow, xhigh, size=(numberofdatapoints,))
+    if xinterval == "regular":
+        x = np.linspace(xlow, xhigh, num=numberofdatapoints)
+    if xinterval == "random":
+        x = np.random.uniform(xlow, xhigh, size=(numberofdatapoints,))
 
-ywithoutnoise = eval(idealcurve)
+    ywithoutnoise = eval(idealcurvecode)
 
-noiseSD = np.amax(ywithoutnoise)*noiselevel
-noise = np.random.normal(0, noiseSD, numberofdatapoints)
+    noiseSD = np.amax(ywithoutnoise)*noiselevel
+    noise = np.random.normal(0, noiseSD, numberofdatapoints)
 
-y = ywithoutnoise + noise
+    y = ywithoutnoise + noise
 
-plt.plot(x, y, 'o')
-plt.show()
+    data = list(zip(x, y))
+    jsonobject = json.dumps(data)
+
+    return jsonobject
+
+if __name__ == "__main__":
+    # User defined variables
+    xlow = 0
+    xhigh = 5
+    xinterval = "random"  # user puts in either "random" or "regular"
+    numberofdatapoints = 10
+    noiselevel = 0.08  # user defines this with a slider
+    idealcurvecode = "8*x**2 + 5*x + 6"
+
+    print(generate_data(xlow, xhigh, xinterval, numberofdatapoints, noiselevel, idealcurvecode))
