11e96eba25d7bb173b589a15112b68dce7d3639d
emmarocheteau <ecr38@cam.ac.uk>
2017-11-25 13:21:32 +0000

    googletranslate working

---
+++ googletranslate/translate_shuffle_functions.py
@@ -0,0 +1,29 @@
+from simple_rest_client.api import API
+from simple_rest_client.resource import Resource
+
+class TranslateResource(Resource):
+    actions = {
+        'translate': {'method': 'POST', 'url': 'language/translate/v2'}
+    }
+
+
+def translate(text, langin, langout, key):
+
+    # set default params
+    default_params = {'key': key, 'model': 'nmt', 'format': "text"}
+    # create api instance
+    translate_api = API(
+        api_root_url='https://translation.googleapis.com', params=default_params,
+        json_encode_body=True
+    )
+
+    translate_api.add_resource(resource_name="translate", resource_class=TranslateResource)
+    output = translate_api.translate.translate(params={'source': langin, 'target': langout, 'q': text}).body
+    return output['data']['translations'][0]['translatedText']
+
+def translateshuffle(text, langin, langout, key):
+
+    translatedtext = translate(text, langin, langout)
+    shuffledtext = translate(translatedtext, langout, langin)
+
+    return shuffledtext
--- plagiarism.py
+++ plagiarism.py
@@ -1,11 +1,22 @@
 """
 Master file for running the entire project
 """
+import re
+import googletranslate.translate_shuffle_functions as tr
+
+#Read in key
+f = open('key.txt', 'r')
+key = f.read()
+f.close()
 
 # Read in file
 f = open('input/it_essay.txt', mode='r')
 essay = f.read()
 f.close()
 # Split into sentences
-sentences = essay.split('.')
+sentences = re.compile("\.|\n").split(essay)
+
+
+
+print(tr.translate(sentences[0], 'en', 'de', key))
 
