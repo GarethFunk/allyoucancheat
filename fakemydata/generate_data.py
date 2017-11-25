bd0432b5552abc549452cb4ee59bd2db1b15fde0
emmarocheteau <ecr38@cam.ac.uk>
2017-11-25 17:54:20 +0000

    googletranslate with text pre-processing

--- fakemydata/generate_data.py
+++ fakemydata/generate_data.py
@@ -1,16 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from decimal import *
 
 #User defined variables
 xlow = 0
 xhigh = 5
 xinterval = "random" #user puts in either "random" or "regular"
-numberofdatapoints = 1000
-getcontext().prec = 3 #decimal precision
-noiselevel =  0.02 #user defines this with a slider
+numberofdatapoints = 10
+precision = 3 #decimal precision
+noiselevel =  0.08 #user defines this with a slider
 idealcurve="8*x**2 + 5*x + 6"
-
 
 if xinterval == "regular":
     x = np.linspace(xlow, xhigh, num=numberofdatapoints)
