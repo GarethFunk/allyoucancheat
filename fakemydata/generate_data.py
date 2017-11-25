eb1eacb703a9f06991f83eb53eea203496cf19ac
emmarocheteau <ecr38@cam.ac.uk>
2017-11-25 19:00:27 +0000

    generate_data function complete. Precision needs to be sorted at the front end

--- fakemydata/generate_data.py
+++ fakemydata/generate_data.py
@@ -1,11 +1,11 @@
 import numpy as np
 import json
 
-def generate_data(xlow, xhigh, xinterval, numberofdatapoints, noiselevel, idealcurvecode):
+def generate_data(xlow, xhigh, xintervalstyle, numberofdatapoints, noiselevel, idealcurvecode):
 
-    if xinterval == "regular":
+    if xintervalstyle == "regular":
         x = np.linspace(xlow, xhigh, num=numberofdatapoints)
-    if xinterval == "random":
+    if xintervalstyle == "random":
         x = np.random.uniform(xlow, xhigh, size=(numberofdatapoints,))
 
     ywithoutnoise = eval(idealcurvecode)
@@ -24,9 +24,9 @@ if __name__ == "__main__":
     # User defined variables
     xlow = 0
     xhigh = 5
-    xinterval = "random"  # user puts in either "random" or "regular"
+    xintervalstyle = "random"  # user puts in either "random" or "regular"
     numberofdatapoints = 10
     noiselevel = 0.08  # user defines this with a slider
     idealcurvecode = "8*x**2 + 5*x + 6"
 
-    print(generate_data(xlow, xhigh, xinterval, numberofdatapoints, noiselevel, idealcurvecode))
+    print(generate_data(xlow, xhigh, xintervalstyle, numberofdatapoints, noiselevel, idealcurvecode))
