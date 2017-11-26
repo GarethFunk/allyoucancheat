a3d171244458251daeaa864e4d08600771149e10
FatQuack <s914247@mvrht.net>
2017-11-26 03:33:53 +0000

    started on graph ui

--- flask/app.py
+++ flask/app.py
@@ -1,41 +1,57 @@
 from flask import Flask, render_template, request
 import json
-from plagiarise_with_translation import plagiarise_with_translation
+
 from fakemydata.generate_data import generate_data
+from plagiarise_with_translation import plagiarise_with_translation
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
+
 @app.route('/fakemydata')
 def fakemydata():
-    #Request the following information from the user:
-    #xlow - lowest x, often 0
-    #xhigh - highest x
-    #xintervalstyle - only takes 2 values, "regular" and "random"
-    #numberofdatapoints
-    #noiselevel - a value between 0 and 1, as determined by user on a sliding scale
-    #idealcurvecode - python code for y
-    #data_json, imagepath = generate_data(xlow, xhigh, xintervalstyle, numberofdatapoints, noiselevel, idealcurvecode)
-    #json is a list of lists (each sublist has one x and one y in it)
-    return data_json, render_template('fakemydata.html')
+    # Request the following information from the user:
+    # xlow - lowest x, often 0
+    # xhigh - highest x
+    # xintervalstyle - only takes 2 values, "regular" and "random"
+    # numberofdatapoints
+    # noiselevel - a value between 0 and 1, as determined by user on a sliding scale
+    # idealcurvecode - python code for y
+    # data_json = generate_data(xlow, xhigh, xintervalstyle, numberofdatapoints, noiselevel, idealcurvecode)
+    # json is a list of lists (each sublist has one x and one y in it)
+    return render_template('graph.html')
+
+
+@app.route('/getdata', methods=['POST'])
+def generatedata():
+    data, imgpath = generate_data(float(request.form["x-min"]),
+                         float(request.form["x-max"]),
+                         request.form["x-interval-style"],
+                         int(request.form["num-points"]),
+                         float(request.form["noise-level"]),
+                         request.form["ideal-curve"])
+    return data
+
 
 @app.route('/sentenceview')
 def sentenceview():
     return render_template('sentenceview.html')
 
+
 @app.route('/plagiarise', methods=['POST'])
 def plagiarise():
-
     essay = request.form["essay"]
     shuffledessay = plagiarise_with_translation(essay)
 
-    #convert essay to json file
+    # convert essay to json file
     essay_json = json.dumps(shuffledessay)
 
     return essay_json
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- flask/static/js/js.js
+++ flask/static/js/js.js
@@ -15,9 +15,33 @@ function update(essay) {
 }
 
 $(function() {
-    ace.edit("editor");
-
     $("#pl-btn").click(function() {
         plagiarise(editor.getValue());
+    });
+
+    $('#noise-level').slider({
+        formatter: function(value) {
+            return 'Noise Level: ' + value;
+        }
+    });
+
+    $("#get-data").click(function() {
+        $.ajax({
+            type: "POST",
+            url: "/getdata",
+            data: {
+                'graph-name': $("#graph-name").val(),
+                'ideal-curve': $("#ideal-curve").val(),
+                'x-min': $("#x-min").val(),
+                'x-max': $("#x-max").val(),
+                'x-interval-style': $("#x-interval-style").val(),
+                'num-points': $("#num-points").val(),
+                'noise-level': $("#noise-level").val()
+            },
+            success: function(r) {
+                console.log(JSON.parse(r));
+
+            }
+        });
     });
 });
